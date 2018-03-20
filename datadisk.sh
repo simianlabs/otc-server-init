479d8ad4a4a54dab5cdd7c0420926ef865ba8086
Stefan Monko <stefan.monko@posam.sk>
2018-03-20 10:38:28 +0100

    convert clrf to lf
    Signed-off-by: Stefan Monko <stefan.monko@posam.sk>

--- datadisk.sh
+++ datadisk.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo "*** Formating dist ****"
+echo "*** Formating dist ****\n"
 
 vgcreate DataGroup /dev/xvdb
+echo "\n"
 lvcreate DataGroup –n VolOpt –L $1G
+echo "\n"
 mkfs –t ext4 /dev/DataGroup/VolOpt
-echo “/dev/DataGroup/VolOpt /opt/ ext4 defaults,noatime,nodiratime 0 0” >> /etc/fstab
-echo "*** Disk DONE ****"
+echo "\n"
+echo "/dev/DataGroup/VolOpt /opt/ ext4 defaults,noatime,nodiratime 0 0" >> /etc/fstab
+
+echo "\n"
+echo "*** Disk DONE ****\n"
 
