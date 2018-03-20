3018b269087023b286daa4f11e303ffb3e613dbc
Stefan Monko <stefan.monko@posam.sk>
2018-03-20 10:40:16 +0100

    command fix for mounts
    Signed-off-by: Stefan Monko <stefan.monko@posam.sk>

--- datadisk.sh
+++ datadisk.sh
@@ -3,9 +3,9 @@ echo "*** Formating dist ****\n"
 
 vgcreate DataGroup /dev/xvdb
 echo "\n"
-lvcreate DataGroup –n VolOpt –L $1G
+lvcreate DataGroup -n VolOpt -L $1G
 echo "\n"
-mkfs –t ext4 /dev/DataGroup/VolOpt
+mkfs -t ext4 /dev/DataGroup/VolOpt
 echo "\n"
 echo "/dev/DataGroup/VolOpt /opt/ ext4 defaults,noatime,nodiratime 0 0" >> /etc/fstab
 
