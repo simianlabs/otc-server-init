52291f64fad9b2169385761056451f45c21ee5be
Stefan Monko <stefan.monko@posam.sk>
2018-03-24 12:34:41 +0100

    Dont format if already ext4
    Signed-off-by: Stefan Monko <stefan.monko@posam.sk>

--- datadisk.sh
+++ datadisk.sh
@@ -5,10 +5,11 @@ vgcreate DataGroup /dev/xvdb
 echo "\n"
 lvcreate DataGroup -n VolOpt -L $1G
 echo "\n"
-mkfs -t ext4 /dev/DataGroup/VolOpt
+echo "n" | mkfs -t ext4 /dev/DataGroup/VolOpt
 echo "\n"
+set -x
 echo "/dev/DataGroup/VolOpt /opt/ ext4 defaults,noatime,nodiratime 0 0" >> /etc/fstab
-
+set +x
 echo "\n"
 echo "*** Disk DONE ****\n"
 
