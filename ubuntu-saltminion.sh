6a0bf60f0792c4276c188e25c7cdb2dc5a9fab54
Stefan Monko <skyneo13@gmail.com>
2018-04-24 21:37:01 +0200

    Update ubuntu-saltminion.sh

--- ubuntu-saltminion.sh
+++ ubuntu-saltminion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 echo "*** Installing SaltStack ***"
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-add-apt-repository "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main""
+add-apt-repository "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main"
 apt-get update
 apt-get -y install salt-minion
 
